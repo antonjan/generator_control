673dcdb400461ef92523317ec33c1a27fbc92c4c
Anton Janovsky <anton.janovsky@gmail.com>
2019-02-04 23:19:52 +0200

    adding working servo sweep

---
+++ Generator_servo_controle_1.iso/Generator_servo_controle_1.iso.ino
@@ -0,0 +1,26 @@
+#include <SoftwareServo.h> 
+SoftwareServo myservo;                      // creates name “my servo” as object to control a servo 
+int pos = 0;                                // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(0);                        // attaches the servo on pin 0 to the servo object 
+} 
+ 
+void loop() 
+{ 
+  for(pos = 0; pos <= 180; pos += 1)        // goes from 0 degrees to 180 degrees 
+  {                                         // in steps of 1 degree 
+    myservo.write(pos);                     // tell servo to go to position in variable 'pos' 
+    delay(15);                              // waits 15 milliseconds for the servo to reach the position
+    SoftwareServo::refresh();               // required by SoftwareServo Library to sweep correctly
+  }           
+  delay (20);                               // required by SoftwareServo Library to sweep correctly
+  for(pos = 180; pos>=0; pos-=1)            // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);                     // tell servo to go to position in variable 'pos' 
+    delay(15);                              // waits 15 milliseconds for the servo to reach the position 
+    SoftwareServo::refresh();               // required by SoftwareServo Library to sweep correctly
+  }
+  delay(20);                                // required by SoftwareServo Library to sweep correctly
+}
---
+++ servo_test1.ino/servo_test1.ino.ino
@@ -0,0 +1,33 @@
+#include <SoftRcPulseOut.h>
+
+SoftRcPulseOut servo;
+int pos = 0;
+#define REFRESH_PERIOD_MS 20
+#define NOW               1
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  //pinMode(0, INPUT); // switch
+  pinMode(0, OUTPUT); // servo pulse and green LED
+  //pinMode(2, INPUT); // potmeter
+  servo.attach(0);
+  servo.setMaximumPulse(2200);  
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  
+    for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+    {                                  // in steps of 1 degree 
+      servo.write(pos);              // tell servo to go to position in variable 'pos' 
+      delay(REFRESH_PERIOD_MS);        // waits 20ms for refresh period 
+      SoftRcPulseOut::refresh(NOW);    // generates the servo pulse Now
+    } 
+    for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+    {                                
+      servo.write(pos);              // tell servo to go to position in variable 'pos' 
+      delay(REFRESH_PERIOD_MS);        // waits 20ms for for refresh period 
+      SoftRcPulseOut::refresh(NOW);    // generates the servo pulse Now
+    } 
+  }
